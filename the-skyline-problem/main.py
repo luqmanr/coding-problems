7878425d457f3389cd64b52bb0429cdd8e0eb580
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2022-10-21 19:05:37 +0700

    huh, didn't know python can create set(str)

--- the-skyline-problem/main.py
+++ the-skyline-problem/main.py
@@ -5,7 +5,6 @@ class Solution:
         for char in chars:
             count = [z for z in s if z == char]
             counts.append(len(count))
-        counts = set(counts)
-        if len(counts) > 1:
+        if len(set(counts)) > 1:
             return False
         return True
