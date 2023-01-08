935713d7dee8b4c892d295f9f68772a4f3112bb7
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2023-01-09 06:18:33 +0700

    add problem to readme

--- gas-station/main.go
+++ gas-station/main.go
@@ -2,7 +2,9 @@
 /*
 1. Check if sum of first_array and second_array is the same, otherwise, there won't be a solution
 2. This is probably a BFS problem, so we need to start searching from each level of gas station traversal
-
+2. Wait no. Before that, we need to find all the indices where gas[i] >= cost[i]
+3. Because if gas[i] < cost[i], the car can't move from the starting gas station
+4. In other words, we need to find the starting point
 */
 package main
 
@@ -25,7 +27,20 @@ func canCompleteCircuit(gas []int, cost []int) int {
 	// if gas sum != cost sum, it's impossible to complete the circuit
 	if gasSum != costSum {
 		return -1
-	} 
+	}
+
+	// find possible starting points
+	startingStations := []int{}
+	for i, g := range gas {
+		if g >= cost[i] {
+			startingStations = append(startingStations, i)
+		}
+	}
+	fmt.Printf("possible starting indices: %d\n", startingStations)
+
+	// for each indices, start a bfs
+	// NOTE: REFRESH ON BFS -> THIS IS A BASIC ALGORITHM, SHOULD BE ON THE BACK OF MY MIND
+	// queue := []int{}
 
 	return 0
 }
