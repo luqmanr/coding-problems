4528aa7160109aa45371e2af7925ca4ccda6cfef
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2023-01-12 15:21:28 +0700

    latest ver

--- karatsuba/main.go
+++ karatsuba/main.go
@@ -5,13 +5,11 @@ import (
 	"math"
 )
 
-func getDecimalDigits(num int) uint {
-	var result uint
-
+func GetDecimalDigits(num int) uint {
+	result := uint(0)
 	if num == 0 {
 		return 1
 	}
-
 	if num < 0 {
 		num = -num
 	}
@@ -23,10 +21,14 @@ func getDecimalDigits(num int) uint {
 	return result
 }
 
-func getHighAndLowDigits(num int, digits uint) (int, int) {
+// to get high (first half) and low (second half) of a number
+// we first divide number by 10^(numDigits/2)
+// so ```a,b := n / 10^(nDigits/2)````
+// we return the divison as the high, and the remainder as the low
+func GetHighAndLowDigits(num int, digits uint) (int, int) {
 	divisor := int(math.Pow(10, float64(digits)))
 	if num >= divisor {
-		fmt.Println(num / divisor, num % divisor)
+		fmt.Println(num, divisor, digits, num / divisor, num % divisor)
 		return num / divisor, num % divisor
 	} else {
 		fmt.Println(0, num)
@@ -35,33 +37,28 @@ func getHighAndLowDigits(num int, digits uint) (int, int) {
 }
 
 func Karatsuba(x int, y int) int {
-	var max_digits uint
-
-	if x == 0 || y == 0 {
-		return 0
-	}
-
+	maxDigits := uint(0)
 	if x < 10 || y < 10 {
 		return x * y
 	}
 
-	x_digits := getDecimalDigits(x)
-	y_digits := getDecimalDigits(y)
+	x_digits := GetDecimalDigits(x)
+	y_digits := GetDecimalDigits(y)
 
 	if x_digits >= y_digits {
-		max_digits = x_digits / 2
+		maxDigits = x_digits / 2
 	} else {
-		max_digits = y_digits / 2
+		maxDigits = y_digits / 2
 	}
 
-	x_high, x_low := getHighAndLowDigits(x, max_digits)
-	y_high, y_low := getHighAndLowDigits(y, max_digits)
+	x_high, x_low := GetHighAndLowDigits(x, maxDigits)
+	y_high, y_low := GetHighAndLowDigits(y, maxDigits)
 
 	z0 := Karatsuba(x_low, y_low)
 	z1 := Karatsuba((x_low + x_high), (y_low + y_high))
 	z2 := Karatsuba(x_high, y_high)
 
-	return (z2 * int(math.Pow(10, float64(2*max_digits)))) + (z1-z2-z0)*int(math.Pow(10, float64(max_digits))) + z0
+	return (z2 * int(math.Pow(10, float64(2*maxDigits)))) + (z1-z2-z0)*int(math.Pow(10, float64(maxDigits))) + z0
 }
 
 func main() {
