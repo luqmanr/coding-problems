1f62399c7ad8cb33ae41e8b2953e37caa4f69c63
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2023-01-20 19:49:43 +0700

    turns out the base case just haven't been implemented properly

--- merge-sort/rewrite2.go
+++ merge-sort/rewrite2.go
@@ -4,20 +4,12 @@ import "fmt"
 
 func MergeSort(toSort []int) (sorted []int) {
 	arrLen := len(toSort)
-	if arrLen <= 2 {
-		if toSort[0] < toSort[1] {
-			sorted = toSort
-			print(sorted)
-			return
-		} else {
-			sorted = []int{toSort[1], toSort[0]}
-			print(sorted)
-			return
-		}
+	if arrLen < 2 {
+		sorted = toSort
+		return
 	}
 	firstHalf := toSort[:arrLen/2]
 	secondHalf := toSort[arrLen/2:]
-	fmt.Println(toSort, firstHalf, secondHalf)
 	sortedFirstHalf := MergeSort(firstHalf)
 	sortedSecondHalf := MergeSort(secondHalf)
 	sorted = Merge(sortedFirstHalf, sortedSecondHalf)
@@ -30,11 +22,9 @@ func Merge(first, second []int) (sorted []int) {
 	for i < len(first) && j < len(second) {
 		if first[i] < second[j] {
 			sorted = append(sorted, first[i])
-			print(sorted)
 			i += 1
 		} else {
 			sorted = append(sorted, second[j])
-			print(sorted)
 			j += 1
 		}
  	}
@@ -53,6 +43,6 @@ func Merge(first, second []int) (sorted []int) {
 
 func main() {
 	// testcase 1
-	res1 := MergeSort([]int{1,6,4,9,3,2,8,7,5})
+	res1 := MergeSort([]int{11,6,4,9,3,2,8,7,5,1,10})
 	fmt.Println(res1)
 }
