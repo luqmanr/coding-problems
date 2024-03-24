ef84d2794e273591e2200ae8f3091619bb256d24
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2024-03-24 12:43:29 +0700

    rewrite again

--- bisection-search/main.py
+++ bisection-search/main.py
@@ -1,23 +1,22 @@
 def bisection_search(L, e):
     def helper(L, e, low, high):
-        # find mid point
+        if low == high:
+            if L[low] == e:
+                return True, low
+            else:
+                return False, None
+        
         mid = (low + high) // 2
-        if mid == low:
-            if e == L[mid]:
-                return True, mid
-
         if L[mid] == e:
             return True, mid
-        elif e > L[mid]:
-            if mid == high:     # nothing left to search
-                return False, None
-            return helper(L, e, mid + 1, high)
+        elif L[mid] < e:
+            # print(f'using high {low, mid, high}')
+            return helper(L, e, mid+1, high)
         else:
-            if low == mid:
-                return False, None
-            return helper(L, e, low, mid - 1)
-    
-    return helper(L, e, 0, len(L) - 1)
+            # print(f'using low {low, mid, high}')
+            return helper(L, e, low, mid-1)
+
+    return helper(L, e, 0, len(L)-1)
 
 if __name__ == '__main__':
     L = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
