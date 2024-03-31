5a5fb15f30f2019979348e7bc2bf68fa36d5d987
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2024-03-31 10:03:42 +0700

    add algorith logic

--- merge-sort/main.py
+++ merge-sort/main.py
@@ -1,42 +1,45 @@
+"""
+divide a list until it is of length 1
+then sort and merge each 2 list to each other
+by comparing each items in the list
+then do it recursively until we get the final list
+"""
 def merge_sort(L):
+    """
+    iterate over two sorted list
+    compare each element in the list each iteration,
+    until one of the list is at the end
+    then copy the rest
+    """
     def merge(low, high):
-        sorted_list = []
-        l = 0
-        h = 0
+        i = 0
+        j = 0
 
-        # iterate over both list, and compare if the one compared is smaller or bigger
-        while l < len(low) and h < len(high):
-            if low[l] < high[h]:
-                sorted_list.append(low[l])
-                l += 1
+        result = []
+        while i < len(low) and j < len(high):
+            if low[i] < high[j]:
+                result.append(low[i])
+                i += 1
             else:
-                sorted_list.append(high[h])
-                h += 1
+                result.append(high[j])
+                j += 1
         
-        # assume both low and high are sorted list
-        # so if we've iterated over one of the list,
-        # we just need to copy the rest of the items in the list, to the new list
-        while l < len(low):
-            sorted_list.append(low[l])
-            l += 1
+        while i < len(low):
+            result.append(low[i])
+            i += 1
+
+        while j < len(high):
+            result.append(high[j])
+            j += 1
         
-        while h < len(high):
-            sorted_list.append(high[h])
-            h += 1
-        
-        return sorted_list
+        return result
     
-    # if only 1 element is in the list,
-    # the by definition, the list is sorted
     if len(L) == 1:
         return L
-
-    # recursively call merge_sort() function on both low & high
-    # until the condition where len(L) == 1
-    mid_idx = len(L) // 2
-    low = merge_sort(L[:mid_idx])
-    high = merge_sort(L[mid_idx:])
-
+    
+    mid = len(L) // 2
+    low = merge_sort(L[:mid])
+    high = merge_sort(L[mid:])
     return merge(low, high)
 
 if __name__ == '__main__':
